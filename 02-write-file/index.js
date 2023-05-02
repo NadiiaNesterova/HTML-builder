81cc327258928c63d6dc1da1004f12fad12bb9d9
Nadiia Nesterova <nesterova1717@gmail.com>
2023-05-03 01:15:37 +0300

    feat: update 02-write-file/index.js file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -3,30 +3,33 @@ const fs = require('fs');
 
 const rl = readline.createInterface({
   input: process.stdin,
-  text: process.stdout
+  output: process.stdout
 });
 
 const filename = './02-write-file/text.txt';
 const fileStream = fs.createWriteStream(filename, { flags: 'a' });
 
-if (!fs.existsSync(filename)) {
-  fs.writeFileSync(filename, '');
-  console.log(`File ${filename} created`);
-}
-
-console.log('Hello! Write something... (type "exit" to exit):');
-
-rl.on('line', (input) => {
-  if (input === 'exit') {
-    console.log('Goodbye!');
-    process.exit(0);
-  } else {
-    console.log(`You entered: ${input}`);
-    fileStream.write(input + '\n');
+fs.access(filename, (err) => {
+  if (err) {
+    fileStream.end();
+    console.log(`File ${filename} created`);
   }
 });
 
-process.on('SIGINT', () => {
+console.log('Hello! Write something... (type "exit" to exit):');
+
+rl.on('line', async (input) => {
+  if (input === 'exit') {
+    console.log('Goodbye!');
+    fileStream.end();
+    process.exit(0);
+  } else {
+    console.log(`You entered: ${input}`);
+    await fs.promises.writeFile(filename, input + '\n', { flag: 'a' });
+  }
+});
+
+rl.on('SIGINT', () => {
   console.log('\rGoodbye!');
   fileStream.end();
   process.exit(0);
