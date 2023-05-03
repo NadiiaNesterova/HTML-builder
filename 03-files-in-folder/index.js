103574df5d3a35514f7e00265a8fe34da2b6ea9b
Nadiia Nesterova <nesterova1717@gmail.com>
2023-05-03 09:05:26 +0300

    feat: update 03-files-in-folder/index.js file

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -3,22 +3,23 @@ const path = require('path');
 
 const directory = './03-files-in-folder/secret-folder';
 
-fs.readdir(directory, (err, files) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
+async function readDirectory() {
+  try {
+    const files = await fs.promises.readdir(directory);
+    for (const file of files) {
+      const filePath = path.join(directory, file);
+      const fileStats = await fs.promises.stat(filePath);
+      if (fileStats.isFile()) {
+        const name = path.basename(file, path.extname(file));
+        const extension = path.extname(file).substring(1);
+        const sizeInBytes = fileStats.size;
 
-  files.forEach((file) => {
-    const filePath = path.join(directory, file);
-    const fileStats = fs.statSync(filePath);
-
-    if (fileStats.isFile()) {
-      const name = path.basename(file, path.extname(file));
-      const extension = path.extname(file).substring(1);
-      const sizeInBytes = fileStats.size;
-
-      console.log(`${name} - ${extension} - ${sizeInBytes} bytes`);
+        console.log(`${name} - ${extension} - ${sizeInBytes} bytes`);
+      }
     }
-  });
-});
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+readDirectory();
